a65b0ee198ffc91883a8bc1b20444e6e87e19b8f
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2022-06-29 16:18:13 +0300

    client

--- dist/main.js
+++ dist/main.js
@@ -4,12 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
+const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix('api');
     const PORT = process.env.PORT || 5000;
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.enableCors();
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'client'));
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Vinyl store')
         .setDescription('The store API description')
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // const app = await NestFactory.create<NestExpressApplication>(
-  //   AppModule,
-  // );
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   const PORT = process.env.PORT || 5000;
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'client'));
 
   //-----documentation
   const config = new DocumentBuilder()
